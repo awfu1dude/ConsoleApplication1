0cbb337a9f3146e9056f3047c271fa7c67d81d85
awfuldude <maxx0n.walk@gmail.com>
2025-07-07 23:41:10 +0700

    added doors and items logic

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,71 +1,154 @@
 #include <iostream>
 #include <vector>
 
-void security_in_club() {
-	int age;
-	std::cin >> age;
-
-	if (age < 18) {
-		std::cout << "entrance denied";
-	}
-	else {
-		std::cout << "you're welcome";
-	}
-}
-
-void guess_number() {
-	srand(0);
-	int a, b;
-	a = rand() % 10;
-	std::cout << "Guess number from 0 to 10 (correct: " << a << "):\n";
-	std::cin >> b;
-	if (a == b) {
-		std::cout << "you win";
-	}
-	else {
-		std::cout << "you lose";
-	}
-}
-
 struct location {
 	std::string name;
 	std::string description;
 	std::vector<int> doors;
+	std::vector<std::string> items;
 };
 
-void move_through_locations() {
-	location locations[10];
-	for (int i = 0; i < 10; i++) {
-		locations[i].name = "Location " + i;
-		locations[i].description = "Description " + i;
-		locations[i].doors.push_back(i);
-		locations[i].doors.push_back(i / 2);
+void print_locations_list(location locations[], int count) {
+	for (int i = 0; i < count; i++) {
+		std::cout << (i + 1) << ". " << locations[i].name << ": " << locations[i].description << std::endl;
 	}
+}
+
+
+void text_adventure() {
+	const int locationsCount = 4;
+	location locations[locationsCount] = { 
+		{
+			"Hallway", 
+			"Hallway description", 
+			{1, 2, 3,},
+			{"Key", "Paper",},
+		},
+		{
+			"Bathroom", 
+			"Bathroom description", 
+			{0, 2,},
+			{"Soap"},
+		},
+		{
+			"Bedroom", 
+			"Bedroom description", 
+			{0, 1,},
+			{},
+		},
+		{
+			"Livingroom", 
+			"Livingroom description", 
+			{0}, 
+			{"Smartphone", "Laptop",},
+		},
+	};
+
+	std::cout << "# Text Adventure #\n\nAvailabe locations for move:\n";
+	print_locations_list(locations, locationsCount);
 
 	int locationIndex = 0;
-	std::string userInput;
-	std::cout << "Enter location name: ";
-	std::cin >> userInput;
-	for (int i = 0; i < 10; i++) {
-		if (locations[i].name == userInput) {
-			locationIndex = i;
-			break;
-		}
-	}
-	if (locationIndex == -1) {
-		std::cout << "Error";
-	}
-	else {
-		for (int i = 0; i < locations[locationIndex].doors.size(); i++) {
+	std::vector<std::string> backpack = {"Pizza"};
+	std::string userInput, item;
 
+	std::cout << "\nYou on location: " << locations[locationIndex].name << std::endl;
+	while (true) {
+		std::cout << "\nEnter location name or command: ";
+		std::cin >> userInput;
+		if (std::cin.peek() != '\n' && std::cin >> item) {
+			// Pick item from location
+			if (userInput == "Pick") {
+				if (item.empty()) {
+					std::cout << "\nYou must print item name.\n";
+				}
+				else if (locations[locationIndex].items.empty()) {
+					std::cout << "\nNo more items on location.\n";
+				}
+				else {
+					for (int i = 0; i < locations[locationIndex].items.size(); i++) {
+						if (i == locations[locationIndex].items.size()) {
+							std::cout << "\nItem not finded.\n";
+							break;
+						}
+						else if (locations[locationIndex].items[i] == item) {
+							backpack.push_back(item);
+							auto iter = locations[locationIndex].items.cbegin();
+							locations[locationIndex].items.erase(iter + i);
+							break;
+						}
+					}
+				}
+			}
+			// Drop item from backpack
+			else if (userInput == "Drop") {
+				if (item.empty()) {
+					std::cout << "\nYou must print item name.\n";
+				}
+				else if (backpack.empty()) {
+					std::cout << "\nYou don't have items in your backpack.\n";
+				}
+				else {
+					for (int i = 0; i < backpack.size() + 1; i++) {
+						if (i == backpack.size()) {
+							std::cout << "\nItem not finded.\n";
+							break;
+						}
+						else if (backpack[i] == item) {
+							locations[locationIndex].items.push_back(item);
+							auto iter = backpack.cbegin();
+							backpack.erase(iter + i);
+							break;
+						}
+					}
+				}
+			}
 		}
-		std::cout << "You on location:\n";
-		std::cout << locations[locationIndex].name;
-		std::cout << locations[locationIndex].description << std::endl;
+		else {
+			// List items on location
+			if (userInput == "List") {
+				std::cout << "\nItems on location " << locations[locationIndex].name << ":\n";
+				if (locations[locationIndex].items.empty()) {
+					std::cout << "Items not finded.\n";
+				}
+				else {
+					for (int i = 0; i < locations[locationIndex].items.size(); i++) {
+						std::cout << (i + 1) << ". " << locations[locationIndex].items[i] << std::endl;
+					}
+				}
+			}
+			// List items in your backpack
+			else if (userInput == "Trunk") {
+				std::cout << "\nItems in your backpack:\n";
+				if (backpack.empty()) {
+					std::cout << "Your backpack is empty.\n";
+				}
+				else {
+					for (int i = 0; i < backpack.size(); i++) {
+						std::cout << (i + 1) << ". " << backpack[i] << std::endl;
+					}
+				}
+			}
+			// Go to location
+			else {
+				for (int i = 0; i < locations[locationIndex].doors.size() + 1; i++) {
+					if (i == locations[locationIndex].doors.size()) {
+						std::cout << "Location not finded.\n\n";
+						break;
+					}
+					int doorIndex = locations[locationIndex].doors[i];
+					if (locations[doorIndex].name == userInput) {
+						locationIndex = doorIndex;
+						break;
+					}
+				}
+				std::cout << "You on location: " << locations[locationIndex].name << std::endl;
+			}
+		}
 	}
 }
 
 int main()
 {
-	move_through_locations();
+	text_adventure();
+	return 0;
 }
